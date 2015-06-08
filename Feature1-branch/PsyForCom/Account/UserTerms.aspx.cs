a8e23b7aecb843cb64b4764411a9cf87c46acd25
Yuval Baumatz <dj_yuval@hotmail.com>
2015-06-08 19:20:07 +0300

    Checkin Feature branch

---
+++ Feature1-branch/PsyForCom/Account/UserTerms.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PsyForCom.Account
+{
+    public partial class UserTerms : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //comment to delete later 
+        }
+
+        protected void RegisterUser(object sender, EventArgs e)
+        {
+            Server.Transfer("UserRegister.aspx", true);
+        }
+    }
+}
